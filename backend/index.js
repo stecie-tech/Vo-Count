20568908d358f8e0d6ee0a2338464606cbeb5802
stecie-tech <76773278+stecie-tech@users.noreply.github.com>
2022-07-08 08:31:19 -0400

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 4300;
+const port = 4500;
 const bodyParser = require("body-parser");
 const cors = require('cors')
 const swaggerJSDoc = require('swagger-jsdoc');
